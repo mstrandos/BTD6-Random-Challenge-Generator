cd27aba795af928736012da5f91687fd8aef7bd2
mstrandos <37367787+mstrandos@users.noreply.github.com>
2018-11-21 17:00:01 +0100

    Update randomizer.py

--- randomizer.py
+++ randomizer.py
@@ -3,3 +3,9 @@ from random import *
 Heroes = ["Gwendolyn", "Striker Jones", "No"]
 
 print(Heroes[randint(0, 2)])
+
+while True:
+  counter = 0
+  counter = counter + 1
+  if counter > 1000:
+    break
